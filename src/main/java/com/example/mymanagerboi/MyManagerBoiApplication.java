9024dcbe3599eefe25e3de05c929053cffd5a7c3
Samicheaterb <35754532+Samicheaterb@users.noreply.github.com>
2023-06-13 09:19:56 -0500

    first comit

---
+++ src/main/java/com/example/mymanagerboi/MyManagerBoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymanagerboi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyManagerBoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyManagerBoiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mymanagerboi/MyManagerBoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymanagerboi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyManagerBoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
